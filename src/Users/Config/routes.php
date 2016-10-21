a172a9884cc97c743448504b18a1bcb8f4e2e7ae
olivernight <work@gallas.me>
2016-10-21 19:10:08 +0200

    Correction des routes

--- src/Users/Config/routes.php
+++ src/Users/Config/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->get('/users/list', 'users.controller:listAction')->bind('users.list');
-$app->get('/users/edit/{id}', 'users.controller::editAction')->bind('users.edit');
-$app->get('/users/new', 'users.controller::newAction')->bind('users.new');
-$app->post('/users/delete/{id}', 'users.controller::deleteAction')->bind('users.delete');
-$app->post('/users/save', 'users.controller::saveAction')->bind('users.save');
+$app->get('/users/edit/{id}', 'users.controller:editAction')->bind('users.edit');
+$app->get('/users/new', 'users.controller:newAction')->bind('users.new');
+$app->post('/users/delete/{id}', 'users.controller:deleteAction')->bind('users.delete');
+$app->post('/users/save', 'users.controller:saveAction')->bind('users.save');
